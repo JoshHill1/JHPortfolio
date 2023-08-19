e7c280acf08441f1b0d76c1ed603d308be970240
Jdog2001 <134169267+JoshHill1@users.noreply.github.com>
2023-08-19 06:25:58 -0700

    Changed back to old look & added stuff in about me

--- navLinks/About/script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".multiple-text", {
-  strings: ["Frontend Developer", "Cyclist", "Gamer", "Digital Artist"],
-  typeSpeed: 100,
-  backSpeed: 85,
-  backDelay: 1000,
-  loop: true
-})
