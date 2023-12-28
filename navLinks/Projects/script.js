6985c9da9a3362f0713052674b3b86c3a8a407b5
Jdog2001 <134169267+JoshHill1@users.noreply.github.com>
2023-12-27 22:49:35 -0800

    Finished layout of homepage; added text, styinging and JS to Projects tab

--- navLinks/Projects/script.js
+++ navLinks/Projects/script.js
@@ -7,26 +7,55 @@ const japanBlogBtn = document.querySelector('#japanBlog');
 const imgDisplay = document.querySelector('#imgDisplay')
 
 
-// unused img's in the file that will change upon hover
+// img in the file that change on hover
 const PProjDisplay = '../../zassets/READMEimg/Screenshot2023-08-22PPF.png';
 const foodMakerProjDisplay = '../../zassets/Images/Projects/FoodMakerApp.png';
 const JapanBlogProjDisplay = '../../zassets/Images/Projects/JJapanBlog.png';
 const PASSProjDisplay = '../../zassets/Images/Projects/PlainAndSimpleForWebProjInvertedColor.png';
 
 // Add event listeners for hover events
-
 portfolioBtn.addEventListener("mouseenter", () => {
   imgDisplay.src = PProjDisplay;
 });
-
 foodMakerBtn.addEventListener("mouseenter", () => {
   imgDisplay.src = foodMakerProjDisplay;
 });
-
 PASSBtn.addEventListener("mouseenter", () => {
   imgDisplay.src = PASSProjDisplay;
 });
-
 japanBlogBtn.addEventListener("mouseenter", () => {
   imgDisplay.src = JapanBlogProjDisplay;
+});
+
+
+
+
+// Function to hide all .processes elements
+function hideAllProcesses() {
+  document.querySelectorAll('.processes').forEach(div => {
+    div.classList.remove('active');
+  });
+}
+
+// Function to show the relevant .processes element
+function showProcess(paraId) {
+  hideAllProcesses();
+  const processDiv = document.querySelector(`#${paraId}`);
+  if (processDiv) {
+    processDiv.classList.add('active');
+  }
+}
+
+// Add event listeners for hover events on links
+document.querySelectorAll('#flexLinks a').forEach(link => {
+  link.addEventListener('mouseenter', () => {
+    const paraId = link.getAttribute('data-para-id');
+    showProcess(paraId);
+
+    // Your existing code for image display
+    const imageSrc = link.getAttribute('data-img-src'); // Add this attribute to your <a> tags
+    if (imgDisplay && imageSrc) {
+      imgDisplay.src = imageSrc;
+    }
+  });
 });
