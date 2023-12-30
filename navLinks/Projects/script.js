709279f5da3e98c4f86f6b7a50552bd23e97455b
Jdog2001 <134169267+JoshHill1@users.noreply.github.com>
2023-12-30 01:22:20 -0800

    Added more to projects page and added localStorage in JS to remember what proj user was last viewing

--- navLinks/Projects/script.js
+++ navLinks/Projects/script.js
@@ -1,4 +1,4 @@
-// getting the element id's
+// Getting element id's
 const portfolioBtn = document.querySelector('#portfolio');
 const foodMakerBtn = document.querySelector('#foodMaker');
 const PASSBtn = document.querySelector('#PASS');
@@ -6,29 +6,54 @@ const japanBlogBtn = document.querySelector('#japanBlog');
 
 const imgDisplay = document.querySelector('#imgDisplay')
 
-
 // img in the file that change on hover
 const PProjDisplay = '../../zassets/READMEimg/Screenshot2023-08-22PPF.png';
 const foodMakerProjDisplay = '../../zassets/Images/Projects/FoodMakerApp.png';
 const JapanBlogProjDisplay = '../../zassets/Images/Projects/JJapanBlog.png';
 const PASSProjDisplay = '../../zassets/Images/Projects/PlainAndSimpleForWebProjInvertedColor.png';
 
-// Add event listeners for hover events
-portfolioBtn.addEventListener("mouseenter", () => {
-  imgDisplay.src = PProjDisplay;
-});
-foodMakerBtn.addEventListener("mouseenter", () => {
-  imgDisplay.src = foodMakerProjDisplay;
-});
-PASSBtn.addEventListener("mouseenter", () => {
-  imgDisplay.src = PASSProjDisplay;
-});
-japanBlogBtn.addEventListener("mouseenter", () => {
-  imgDisplay.src = JapanBlogProjDisplay;
+document.addEventListener("DOMContentLoaded", () => {
+  // Check if there's saved content in localStorage
+  const savedParaId = localStorage.getItem('lastViewedParaId');
+  const savedImageSrc = localStorage.getItem('lastViewedImageSrc');
+
+  if (savedParaId && savedImageSrc) {
+    showProcess(savedParaId);
+    imgDisplay.src = savedImageSrc;
+  }
 });
 
+// Add event listeners for hover events on links
+document.querySelectorAll('#flexLinks a').forEach(link => {
+  link.addEventListener('mouseenter', () => {
+    const paraId = link.getAttribute('data-para-id');
+    showProcess(paraId);
 
+    let imageSrc;
+    switch (paraId) {
+      case 'para1':
+        imageSrc = PProjDisplay;
+        break;
+      case 'para2':
+        imageSrc = foodMakerProjDisplay;
+        break;
+      case 'para3':
+        imageSrc = PASSProjDisplay;
+        break;
+      case 'para4':
+        imageSrc = JapanBlogProjDisplay;
+        break;
+    }
 
+    if (imgDisplay && imageSrc) {
+      imgDisplay.src = imageSrc;
+
+      // Saves current paraId and imageSrc to localStorage
+      localStorage.setItem('lastViewedParaId', paraId);
+      localStorage.setItem('lastViewedImageSrc', imageSrc);
+    }
+  });
+});
 
 // Function to hide all .processes elements
 function hideAllProcesses() {
@@ -45,17 +70,3 @@ function showProcess(paraId) {
     processDiv.classList.add('active');
   }
 }
-
-// Add event listeners for hover events on links
-document.querySelectorAll('#flexLinks a').forEach(link => {
-  link.addEventListener('mouseenter', () => {
-    const paraId = link.getAttribute('data-para-id');
-    showProcess(paraId);
-
-    // Your existing code for image display
-    const imageSrc = link.getAttribute('data-img-src'); // Add this attribute to your <a> tags
-    if (imgDisplay && imageSrc) {
-      imgDisplay.src = imageSrc;
-    }
-  });
-});
